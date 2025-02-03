b48d04fc182ba1cfc635e3279acde31757e6562d
fajarajiprayoga <fajaraji.prayoga@gmail.com>
2025-02-03 09:33:02 +0700

    Up

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Events\RfidMiddlewareEvent;
 use App\Http\Controllers\Api\RfidMiddlewareController;
+use App\Models\RfidData;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +14,25 @@ Route::get('/user', function (Request $request) {
 
 Route::get("/list", [RfidMiddlewareController::class, 'list']);
 Route::post("/store", [RfidMiddlewareController::class, 'store']);
+
+Route::post("/1", function(Request $request){
+    Log::info("Hit Request");
+
+    $validator = Validator::make($request->all(), [
+        'ClientType' => 'required',
+        'TagNo' => 'required',
+        'ReaderNo' => 'required'
+    ]);
+
+    if($validator->fails()){
+        Log::info("Falidation Failed");
+    }else {
+        $rfid_data = new RfidData();
+        $rfid_data->client_type = $request['ClientType'];
+        $rfid_data->tag_no = $request['TagNo'];
+        $rfid_data->reader_no = $request['ReaderNo'];
+        $rfid_data->save();
+        
+        Log::info("Data Stored");
+    }
+});
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\DisplayAbsenceController;
+use App\Models\RfidData;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DisplayAbsenceController::class, 'index']);
+Route::get('/count_db', function(){
+    $count = RfidData::count();
+    dd($count);
+});
